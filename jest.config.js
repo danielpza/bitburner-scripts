1b5554f9655efd250a5ab872670605ebcc89e44e
Daniel Perez Alvarez <danielpza@protonmail.com>
2022-01-19 20:23:19 -0500

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+import _ from "lodash";
+
 /** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
+  globals: { _ },
 };
