1d6f3de9a8bd25a54eb705533b5d0f404359448d
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-10 23:17:48 -0500

    fixes

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,5 +1,6 @@
 declare namespace Bitburner {
   type NS = import("../NetScriptDefinitions.d.ts").NS;
+  type Server = import("../NetScriptDefinitions.d.ts").Server;
   type AutocompleteData =
     import("../NetScriptDefinitions.d.ts").AutocompleteData;
   type Flags = Parameters<AutocompleteData["flags"]>[0];
