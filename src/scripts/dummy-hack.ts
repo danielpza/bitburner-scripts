eaebfe2c6977fcac338af46f1efbf37de2958840
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-11 13:44:25 -0500

    add delay flag dummy-*

--- src/scripts/dummy-hack.ts
+++ src/scripts/dummy-hack.ts
@@ -1,4 +1,10 @@
 export async function main(ns: Bitburner.NS) {
-  const [host] = ns.args as string[];
+  const {
+    _: [host],
+    delay,
+  } = ns.flags([["delay", 0]]) as { _: [string]; delay: number };
+
+  if (delay) await ns.sleep(delay);
+
   await ns.hack(host);
 }
