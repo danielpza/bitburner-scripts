edd85b8ddaa02879dde91b396bd049e354b2ab86
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-11 18:16:25 -0500

    kill-all

---
+++ src/kill-all.ts
@@ -0,0 +1,10 @@
+import { scanAll } from "./utils/scanAll";
+
+export function main(ns: Bitburner.NS) {
+  const servers = scanAll(ns).filter(
+    (server) => ns.hasRootAccess(server) && server !== ns.getHostname(),
+  );
+  for (const server of servers) {
+    ns.killall(server);
+  }
+}
