d79077dda09a64a885dc23acedff57e10ac4b926
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-11 03:54:57 -0500

    small change

--- src/search-and-destroy.ts
+++ src/search-and-destroy.ts
@@ -1,11 +1,14 @@
+import { scanAll } from "./utils/scanAll";
+
 export function main(ns: Bitburner.NS) {
-  const servers = ns.scan().filter((server) => !ns.hasRootAccess(server));
+  const servers = scanAll(ns).filter((server) => !ns.hasRootAccess(server));
   for (const target of servers) {
     try {
       ns.brutessh(target);
     } catch (e) {}
     try {
       ns.nuke(target);
+      ns.tprint(`Nuked ${target}`);
     } catch (e) {}
   }
 }
