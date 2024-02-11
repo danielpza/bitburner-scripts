fecef53190c3178d0ceb7a7b8360cbcd035e5468
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-11 03:55:10 -0500

    missing scripts

---
+++ src/utils/scanAll.ts
@@ -0,0 +1,18 @@
+/**
+ * Returns a list of all servers available in the game
+ *
+ * @example
+ *   const allServers = scanAll(ns);
+ */
+export function scanAll(
+  ns: Bitburner.NS,
+  target = ns.getHostname(),
+  visited = new Set(),
+): string[] {
+  visited.add(target);
+
+  return ns
+    .scan(target)
+    .filter((child) => !visited.has(child))
+    .flatMap((child) => [child, ...scanAll(ns, child, visited)]);
+}
