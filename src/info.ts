1565abe6f8eb5f9597b81885fdf2041a59374f15
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-11 22:54:14 -0500

    update info.ts

--- src/info.ts
+++ src/info.ts
@@ -1,45 +1,78 @@
 import { table } from "./utils/table";
 import { scanAll } from "./utils/scanAll";
 
-export function main(ns: Bitburner.NS) {
-  const servers = scanAll(ns).filter((server) => ns.hasRootAccess(server));
-  const out = table(
-    _.orderBy(servers, [(server) => ns.getServerRequiredHackingLevel(server)]),
-    [
-      {
-        header: "Name",
-        getValue: (host) => host,
-        align: "left",
-      },
-      {
-        header: "Money",
-        getValue: (host) => ns.getServerMoneyAvailable(host),
-        format: ns.formatNumber,
-      },
-      {
-        header: "Max Money",
-        getValue: (host) => ns.getServerMaxMoney(host),
-        format: ns.formatNumber,
-      },
-      {
-        header: "Security",
-        getValue: (host) => ns.getServerSecurityLevel(host),
-        format: ns.formatNumber,
-      },
-      {
-        header: "Min Sec",
-        getValue: (host) => ns.getServerMinSecurityLevel(host),
-        format: ns.formatNumber,
-      },
-      {
-        header: "Skill",
-        getValue: (host) => ns.getServerRequiredHackingLevel(host),
-      },
-      {
-        header: "RAM",
-        getValue: (host) => ns.getServerMaxRam(host),
-      },
-    ],
-  );
-  ns.tprint("\n", out);
+export async function main(ns: Bitburner.NS) {
+  const { loop } = ns.flags([["loop", false]]) as { loop: boolean };
+
+  ns.print(ns.args);
+
+  ns.disableLog("ALL");
+
+  function getInfo() {
+    const servers = scanAll(ns).filter((server) => ns.hasRootAccess(server));
+
+    return table(
+      _.orderBy(servers, [
+        (server) => ns.getServerRequiredHackingLevel(server),
+      ]),
+      [
+        {
+          header: "Name",
+          getValue: (host) => host,
+          align: "left",
+        },
+        {
+          header: "Money",
+          getValue: (host) => ns.getServerMoneyAvailable(host),
+          format: ns.formatNumber,
+        },
+        {
+          header: "Max Money",
+          getValue: (host) => ns.getServerMaxMoney(host),
+          format: ns.formatNumber,
+        },
+        {
+          header: "Security",
+          getValue: (host) => ns.getServerSecurityLevel(host),
+          format: ns.formatNumber,
+        },
+        {
+          header: "Min Sec",
+          getValue: (host) => ns.getServerMinSecurityLevel(host),
+          format: ns.formatNumber,
+        },
+        {
+          header: "Skill",
+          getValue: (host) => ns.getServerRequiredHackingLevel(host),
+        },
+        {
+          header: "RAM",
+          getValue: (host) => ns.getServerMaxRam(host),
+        },
+        {
+          header: "growth",
+          getValue: (host) => ns.getServerGrowth(host),
+        },
+      ],
+    );
+  }
+
+  if (!loop) {
+    ns.tprint("\n", getInfo());
+    return;
+  }
+  ns.isRunning(script);
+
+  ns.tail();
+  ns.resizeTail(800, 400);
+
+  for (;;) {
+    ns.print(
+      "\n",
+      "-------------------------------------------",
+      "\n",
+      getInfo(),
+    );
+    await ns.sleep(1000);
+  }
 }
