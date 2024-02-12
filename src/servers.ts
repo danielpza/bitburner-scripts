a44e4079f5f9715cf6b0f205313a30c1803652fd
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-11 22:18:45 -0500

    update servers.ts

--- src/servers.ts
+++ src/servers.ts
@@ -1,11 +1,28 @@
 import { table } from "./utils/table";
 
+export function autocomplete() {
+  return ["buy", "upgrade", "list"];
+}
+
 export async function main(ns: Bitburner.NS) {
   const [operation] = ns.args;
 
   const servers = ns.getPurchasedServers();
 
-  if (operation === "upgrade") {
+  if (operation === "buy") buy();
+  else if (operation === "upgrade") upgrade();
+  else list();
+
+  function buy() {
+    let ram = getBiggestRam();
+    if (!ram) {
+      ns.tprint("Cannot buy any servers");
+      return;
+    }
+    ns.purchaseServer(getName(), ram);
+  }
+
+  function upgrade() {
     for (;;) {
       const smallestServer = _.minBy(servers, ns.getServerMaxRam);
 
@@ -22,23 +39,41 @@ export async function main(ns: Bitburner.NS) {
     return;
   }
 
-  ns.tprint(
-    "\n",
-    table(
-      servers.map((host) => ({
-        name: host,
-        used: ns.getServerUsedRam(host),
-        total: ns.getServerMaxRam(host),
-      })),
-      [
-        { header: "name", align: "left" },
-        { header: "used", format: ramFormat },
-        { header: "total", format: ramFormat },
-      ],
-    ),
-  );
+  function list() {
+    ns.tprint(
+      "\n",
+      table(
+        servers.map((host) => ({
+          name: host,
+          used: ns.getServerUsedRam(host),
+          total: ns.getServerMaxRam(host),
+        })),
+        [
+          { header: "name", align: "left" },
+          { header: "used", format: ramFormat },
+          { header: "total", format: ramFormat },
+        ],
+      ),
+    );
+  }
 
   function ramFormat(value: number) {
     return ns.formatNumber(value, 0);
+  }
+
+  function getBiggestRam() {
+    let ram: number;
+    for (
+      ram = 1;
+      ns.getPurchasedServerCost(ram) <= ns.getPlayer().money;
+      ram *= 2
+    ) {}
+    if (ram === 1) return 0;
+    ram /= 2;
+    return ram;
+  }
+
+  function getName() {
+    return `purchased_server_${ns.getPurchasedServers().length + 1}`;
   }
 }
