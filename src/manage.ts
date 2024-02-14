def79c1db698339d99a7a7c686af90d88d3378b7
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-14 02:19:28 -0500

    small optimization

--- src/manage.ts
+++ src/manage.ts
@@ -1,8 +1,10 @@
-import { SLEEP } from "./utils/constants.ts";
+import { SLEEP, Script, WEAK_ANALYZE } from "./utils/constants.ts";
 
 import { growTarget } from "./grow.ts";
 import { hackTarget } from "./hack.ts";
 import { weakenTarget } from "./weaken.ts";
+import { getRootAccessServers } from "./utils/getRootAccessServers.ts";
+import { getClusterFreeThreads } from "./utils/getClusterFreeThreads.ts";
 
 export function autocomplete(data: Bitburner.AutocompleteData) {
   return data.servers;
@@ -17,11 +19,37 @@ export async function main(ns: Bitburner.NS) {
   ns.resizeTail(600, 120);
 
   while (true) {
-    await Promise.all([
-      weakenTarget(ns, target),
-      growTarget(ns, target, { extraDelay: SLEEP }),
-    ]);
+    if (canFullyWeaken(target)) {
+      await Promise.all([
+        weakenTarget(ns, target),
+        growTarget(ns, target, { extraDelay: SLEEP }),
+      ]);
+    } else {
+      await weakenTarget(ns, target);
+      await growTarget(ns, target);
+    }
+
     await hackTarget(ns, target);
     await ns.sleep(1500);
+  }
+
+  function canFullyWeaken(target: string) {
+    const currentSecurity = ns.getServerSecurityLevel(target);
+    const minSecurity = ns.getServerMinSecurityLevel(target);
+
+    const secToRemove = currentSecurity - minSecurity;
+
+    if (secToRemove <= 0) {
+      return true;
+    }
+
+    const servers = getRootAccessServers(ns);
+    const freeThreads = getClusterFreeThreads(
+      ns,
+      servers,
+      ns.getScriptRam(Script.WEAKEN),
+    );
+
+    return Math.ceil(secToRemove / WEAK_ANALYZE) <= freeThreads;
   }
 }
