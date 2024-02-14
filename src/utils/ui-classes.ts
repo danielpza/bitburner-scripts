a9c5aef6277685945b1bbbf5dd55ddf0a08328d0
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-14 00:37:20 -0500

    hacky react

---
+++ src/utils/ui-classes.ts
@@ -0,0 +1,5 @@
+const doc = eval("document");
+
+export const BUTTON_CLASS = doc.querySelector(
+  ".MuiButtonBase-root.MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeMedium.MuiButton-textSizeMedium.MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeMedium.MuiButton-textSizeMedium",
+)?.className;
--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -1,9 +1,5 @@
 import { type ReactNode } from "../../NetScriptDefinitions.js";
 
-export const BUTTON_CLASS = document.querySelector(
-  ".MuiButtonBase-root.MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeMedium.MuiButton-textSizeMedium.MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeMedium.MuiButton-textSizeMedium",
-)?.className;
-
 async function keepAlive(ns: Bitburner.NS) {
   while (true) {
     await ns.asleep(10000000);
