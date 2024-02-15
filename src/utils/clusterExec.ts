91e8e999a67855bfd1a9ea2af83e77a211fd4654
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-15 17:40:27 -0500

    update

--- src/utils/clusterExec.ts
+++ src/utils/clusterExec.ts
@@ -1,5 +1,5 @@
 import { getFreeThreads } from "./getFreeThreads.ts";
-import { RemoteExecOptions, remoteExecOld, remoteExec } from "./remoteExec.ts";
+import { RemoteExecOptions, remoteExec } from "./remoteExec.ts";
 
 export interface ClusterExecOptionsOld {
   script: string;
@@ -13,25 +13,11 @@ export function clusterExecOld(
   hosts: string[],
   { script, threads = 1, target, delay = 0 }: ClusterExecOptionsOld,
 ) {
-  let missingThreads = threads;
-  for (const host of hosts) {
-    const freeThreads = getFreeThreads(ns, host, ns.getScriptRam(script));
-    if (freeThreads > 0) {
-      const threadsToUse = Math.min(freeThreads, missingThreads);
-      remoteExecOld(ns, {
-        script,
-        host,
-        threads: threadsToUse,
-        target,
-        delay,
-      });
-      missingThreads -= threadsToUse;
-    }
-    if (missingThreads <= 0) break;
-  }
-  if (missingThreads > 0) {
-    throw new Error("no enough free threads");
-  }
+  return clusterExec(ns, hosts, {
+    threads,
+    script,
+    args: [target, "--delay", delay],
+  });
 }
 
 export function clusterExec(
--- src/utils/remoteExec.ts
+++ src/utils/remoteExec.ts
@@ -1,23 +1,6 @@
 import { RunOptions } from "../../NetScriptDefinitions.js";
 import { ProcessCleanup } from "./ProcessCleanup.ts";
 
-export interface RemoteExecOptionsOld {
-  script: string;
-  host: string;
-  threads?: number;
-  target: string;
-  delay?: number;
-}
-
-export function remoteExecOld(
-  ns: Bitburner.NS,
-  { script, host, threads, target, delay = 0 }: RemoteExecOptionsOld,
-) {
-  ns.scp(script, host);
-  let pid = ns.exec(script, host, { threads }, target, "--delay", delay);
-  ProcessCleanup.get(ns).add(pid);
-}
-
 export interface RemoteExecOptions {
   script: string;
   threads?: RunOptions["threads"];
