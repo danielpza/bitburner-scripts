292302d2ed71a51d0956aeed5675f13c2ec0bb11
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-16 15:33:48 -0500

    start.ts

---
+++ src/start.ts
@@ -0,0 +1,17 @@
+async function start(ns: Bitburner.NS, script: string, args: string[]) {
+  const isRunning = ns.scriptRunning(script, "home");
+
+  if (!isRunning) {
+    const pid = ns.run(script, 1, ...args);
+    await ns.asleep(500);
+    ns.kill(pid);
+  }
+}
+
+export async function main(ns: Bitburner.NS) {
+  await start(ns, "nuke-all.js", ["--loop"]);
+  await start(ns, "brain.js", ["--loop"]);
+  await start(ns, "tor.js", ["--loop"]);
+  await start(ns, "servers.js", ["loop"]);
+  await start(ns, "contracts/find.js", ["--loop"]);
+}
