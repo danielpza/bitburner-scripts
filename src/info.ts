87ea33bfa8e7de328378851686626d3c9ff38560
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-17 14:21:10 -0500

    update

--- src/info.ts
+++ src/info.ts
@@ -1,19 +1,13 @@
 import { scanAll } from "./utils/scanAll.ts";
 import { formatTable } from "./utils/formatTable.ts";
-import { HACK_SKILL_THRESHOLD, Jobs, TARGET_HACK_PERCENT } from "./utils/constants.ts";
+import { GROW_PER_WEAK, HACK_PER_WEAK, HACK_SKILL_THRESHOLD, Jobs, TARGET_HACK_PERCENT } from "./utils/constants.ts";
 import { getRequiredWeakenThreads } from "./weaken.ts";
-import { getClusterFreeThreads } from "./utils/getClusterFreeThreads.ts";
 
 export function autocomplete() {
   return ["--sec", "--ram", "--extra", "--time", "--threads"];
 }
 
-const HACK_ANALYZE_SEC = 0.002; // ns.hackAnalyzeSecurity(1);
-const GROW_ANALYZE_SEC = 0.004; // ns.growthAnalyzeSecurity(1);
-const WEAK_ANALYZE = 0.05; // ns.weakenAnalyze(1);
-
-const GROW_PER_WEAK = WEAK_ANALYZE / GROW_ANALYZE_SEC;
-const HACK_PER_WEAK = WEAK_ANALYZE / HACK_ANALYZE_SEC;
+const TIME_GROUP = 1000 * 60 * 5;
 
 export async function main(ns: Bitburner.NS) {
   const sec = ns.args.includes("--sec");
@@ -22,55 +16,18 @@ export async function main(ns: Bitburner.NS) {
   const time = ns.args.includes("--time");
   const threads = ns.args.includes("--threads");
 
-  const TIME_GROUP = 1000 * 60 * 5;
+  const RAM = ns.getScriptRam(Jobs.Weaken.script);
 
   ns.disableLog("ALL");
 
   function getServers() {
-    const servers = scanAll(ns).filter((server) => ns.getServerMaxMoney(server) > 0 && ns.hasRootAccess(server));
-    const RAM = ns.getScriptRam(Jobs.Weaken.script);
+    const servers = scanAll(ns).filter((server) => ns.hasRootAccess(server));
     const maxThreads = servers.reduce((acc, server) => acc + Math.floor(ns.getServerMaxRam(server) / RAM), 0);
     const playerHackLevel = ns.getHackingLevel();
 
-    return servers.map((server) => {
-      const hackLevel = ns.getServerRequiredHackingLevel(server);
-      const requiredWeaken = getRequiredWeakenThreads(ns, server);
-      const weakenCycles = Math.ceil(requiredWeaken / maxThreads);
-      const weakenTime = ns.getWeakenTime(server);
-
-      const totalWeakenTime = weakenTime * weakenCycles;
-
-      const maxMoney = ns.getServerMaxMoney(server);
-
-      const hackThreads = Math.ceil(TARGET_HACK_PERCENT / ns.hackAnalyze(server));
-      const growThreads = Math.ceil(ns.growthAnalyze(server, 1 / (1 - TARGET_HACK_PERCENT)));
-      const weakThreads = Math.ceil(hackThreads / HACK_PER_WEAK + growThreads / GROW_PER_WEAK);
-
-      const hgwThreads = hackThreads + growThreads + weakThreads;
-
-      const hasSkill = hackLevel < playerHackLevel / HACK_SKILL_THRESHOLD;
-      const moneyPerThread = maxMoney / hgwThreads;
-      const weakenScore = -Math.floor((weakenTime * weakenCycles) / TIME_GROUP);
-      const weakenTimeScore = -Math.floor(weakenTime / TIME_GROUP);
-
-      return {
-        name: server,
-        hackLevel,
-        requiredWeaken,
-        weakenCycles,
-        weakenTime,
-        totalWeakenTime,
-        maxMoney,
-        hackThreads,
-        growThreads,
-        weakThreads,
-        hgwThreads,
-        hasSkill,
-        moneyPerThread,
-        weakenScore,
-        weakenTimeScore,
-      };
-    });
+    return servers
+      .filter((server) => ns.getServerMaxMoney(server) > 0)
+      .map((server) => getServerInfo(ns, server, { maxThreads, playerHackLevel }));
   }
 
   function getInfo() {
@@ -122,4 +79,48 @@ export async function main(ns: Bitburner.NS) {
     ns.print("\n", "-------------------------------------------", "\n", getInfo());
     await ns.sleep(1000);
   }
+}
+
+function getServerInfo(
+  ns: Bitburner.NS,
+  server: string,
+  { maxThreads, playerHackLevel }: { maxThreads: number; playerHackLevel: number },
+) {
+  const hackLevel = ns.getServerRequiredHackingLevel(server);
+  const requiredWeaken = getRequiredWeakenThreads(ns, server);
+  const weakenCycles = Math.ceil(requiredWeaken / maxThreads);
+  const weakenTime = ns.getWeakenTime(server);
+
+  const totalWeakenTime = weakenTime * weakenCycles;
+
+  const maxMoney = ns.getServerMaxMoney(server);
+
+  const hackThreads = Math.ceil(TARGET_HACK_PERCENT / ns.hackAnalyze(server));
+  const growThreads = Math.ceil(ns.growthAnalyze(server, 1 / (1 - TARGET_HACK_PERCENT)));
+  const weakThreads = Math.ceil(hackThreads / HACK_PER_WEAK + growThreads / GROW_PER_WEAK);
+
+  const hgwThreads = hackThreads + growThreads + weakThreads;
+
+  const hasSkill = hackLevel < playerHackLevel / HACK_SKILL_THRESHOLD;
+  const moneyPerThread = maxMoney / hgwThreads;
+  const weakenScore = -Math.floor((weakenTime * weakenCycles) / TIME_GROUP);
+  const weakenTimeScore = -Math.floor(weakenTime / TIME_GROUP);
+
+  return {
+    name: server,
+    hackLevel,
+    requiredWeaken,
+    weakenCycles,
+    weakenTime,
+    totalWeakenTime,
+    maxMoney,
+    hackThreads,
+    growThreads,
+    weakThreads,
+    hgwThreads,
+    hasSkill,
+    moneyPerThread,
+    weakenScore,
+    weakenTimeScore,
+  };
 }
