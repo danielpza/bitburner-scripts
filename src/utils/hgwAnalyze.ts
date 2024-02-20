9592514095f6b5b22e843110b44b37b8ce13d82c
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-02-20 16:21:11 -0500

    update hgwAnalyze

--- src/utils/hgwAnalyze.ts
+++ src/utils/hgwAnalyze.ts
@@ -1,7 +1,9 @@
 import { GROW_PER_WEAK, HACK_PER_WEAK } from "./constants.ts";
 
 export function hgwAnalyze(ns: Bitburner.NS, target: string, targetHackPercent: number) {
-  const hackThreads = Math.ceil(targetHackPercent / ns.hackAnalyze(target));
+  const hackAnalyze = ns.hackAnalyze(target);
+  if (hackAnalyze === 0) return { hackThreads: 0, growThreads: 0, weakenThreads: 0, totalThreads: 0 };
+  const hackThreads = Math.ceil(targetHackPercent / hackAnalyze);
   const growThreads = Math.ceil(ns.growthAnalyze(target, 1 / (1 - ns.hackAnalyze(target) * hackThreads)));
   const weakenThreads = Math.ceil(hackThreads / HACK_PER_WEAK + growThreads / GROW_PER_WEAK);
 
