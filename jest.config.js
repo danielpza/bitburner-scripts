f58369145fa23c481a52561c753837f0f1e8c5b5
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-12-30 01:29:30 -0800

    update config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-import _ from "lodash";
-
-/** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
-export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  moduleFileExtensions: ["js", "ts", "d.ts"],
-  globals: { _ },
-};
