4d4c9671a09c033a51a95f0de4aa3e0fa59fca1d
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-12-30 01:33:20 -0800

    scratch

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,16 +1,8 @@
 declare namespace Bitburner {
   type NS = import("../NetScriptDefinitions.d.ts").NS;
   type Server = import("../NetScriptDefinitions.d.ts").Server;
-  type AutocompleteData =
-    import("../NetScriptDefinitions.d.ts").AutocompleteData;
+  type AutocompleteData = import("../NetScriptDefinitions.d.ts").AutocompleteData;
   type Flags = Parameters<AutocompleteData["flags"]>[0];
-}
-
-declare global {
-  interface Window {
-    React: typeof import("react");
-    ReactDOM: typeof import("react-dom");
-  }
 }
 
 declare const _: typeof import("lodash");
