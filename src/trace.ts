7a39b7ed5e966a476582c65a663d01add4640956
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-12-30 19:47:33 -0800

    add trace --clip

--- src/trace.ts
+++ src/trace.ts
@@ -1,11 +1,21 @@
 import { trace } from "./utils/trace.ts";
 
 export function autocomplete(autocompleteData: Bitburner.AutocompleteData) {
-  return autocompleteData.servers;
+  return [...autocompleteData.servers, "--clip"];
 }
 
 export function main(ns: Bitburner.NS) {
   const target = ns.args[0] as string;
+
+  if (ns.args.includes("--clip")) {
+    ns.tprint(
+      "\n" +
+        trace(ns, target)
+          ?.map((server) => `connect ${server}`)
+          .join("; "),
+    );
+    return;
+  }
 
   ns.tprint(trace(ns, target)?.join(" -> "));
 }
