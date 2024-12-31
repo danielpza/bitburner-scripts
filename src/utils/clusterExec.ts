ad2afe6ff1032eba75ed01329f6bdc854071580c
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-12-30 19:47:53 -0800

    pass abort signals around

--- src/utils/clusterExec.ts
+++ src/utils/clusterExec.ts
@@ -1,21 +1,27 @@
 import { getFreeThreads } from "./getFreeThreads.ts";
-import { RemoteExecOptions, remoteExec } from "./remoteExec.ts";
+import { type RemoteExecOptions, remoteExec } from "./remoteExec.ts";
 
-export function clusterExec(ns: Bitburner.NS, hosts: string[], { script, args, threads = 1 }: RemoteExecOptions) {
+export function clusterExec(
+  ns: Bitburner.NS,
+  hosts: string[],
+  { script, args, threads = 1 }: RemoteExecOptions,
+  { signal }: { signal?: AbortSignal } = {},
+) {
   let missingThreads = threads;
+  let pids: number[] = [];
   for (const host of hosts) {
     const freeThreads = getFreeThreads(ns, host, ns.getScriptRam(script));
     if (freeThreads > 0) {
       const threadsToUse = Math.min(freeThreads, missingThreads);
-      remoteExec(ns, host, {
-        script,
-        threads: threadsToUse,
-        args,
-      });
+      const pid = remoteExec(ns, host, { script, threads: threadsToUse, args });
+      pids.push(pid);
       missingThreads -= threadsToUse;
     }
     if (missingThreads <= 0) break;
   }
+  signal?.addEventListener("abort", () => {
+    for (const pid of pids) ns.kill(pid);
+  });
   if (missingThreads > 0) {
     throw new Error("no enough free threads");
   }
