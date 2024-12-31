1199a07c05156c24fd29a8e35b2e8afb58703030
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-12-30 23:21:23 -0800

    fix globals

--- globals.d.ts
+++ globals.d.ts
@@ -1,11 +1,11 @@
 declare namespace Bitburner {
-  type NS = import("../NetScriptDefinitions.d.ts").NS;
-  type Server = import("../NetScriptDefinitions.d.ts").Server;
-  type AutocompleteData = import("../NetScriptDefinitions.d.ts").AutocompleteData;
+  type NS = import("./NetScriptDefinitions.d.ts").NS;
+  type Server = import("./NetScriptDefinitions.d.ts").Server;
+  type AutocompleteData = import("./NetScriptDefinitions.d.ts").AutocompleteData;
   type Flags = Parameters<AutocompleteData["flags"]>[0];
-  type RunOptions = import("../NetScriptDefinitions.d.ts").RunOptions;
+  type RunOptions = import("./NetScriptDefinitions.d.ts").RunOptions;
 }
 
-declare type NS = import("../NetScriptDefinitions.d.ts").NS;
+declare type NS = import("./NetScriptDefinitions.d.ts").NS;
 declare type React = import("react");
 declare const _: typeof import("lodash");
